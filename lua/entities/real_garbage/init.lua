692af36cc06b1945b3dab7f61d627e9c376040e8
MrMarrant <laughymclaugher@gmail.com>
2023-07-01 11:34:06 +0200

    [INIT] Intial commit

---
+++ lua/entities/real_garbage/init.lua
@@ -0,0 +1,101 @@
+-- Real Garbage, A representation of a Garbage object on the game Garry's Mod.
+-- Copyright (C) 2023  MrMarrant aka BIBI.
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+-- TODO : Trouver les sons.
+local PhysicSoundLow = Sound( "physics/glass/glass_bottle_impact_hard"..math.random(1, 3)..".wav" )
+local PhysicSoundHeavy = Sound( "physics/glass/glass_bottle_break"..math.random(1, 2)..".wav" )
+local TrowSoundGarbage = Sound( "" )
+local OpenSoundGarbage = Sound( "" )
+
+function ENT:AddTrash(ent)
+	if (ent:IsPlayer() or ent:IsWorld()) then return end
+
+	table.insert( self.Trash, { -- TODO : Save BodyGroup ?
+		class = ent:GetClass(),
+		model = ent:GetModel(),
+		health = ent:Health()
+	})
+
+	self.ActualCapacity = self.ActualCapacity + 1
+	ent:Remove()
+end
+
+function ENT:RemoveTrash()
+	if (table.IsEmpty( self.Trash )) then return end
+
+	local TrashToRemove = self.Trash[#]
+		
+	local TrashEnt = ents.Create( TrashToRemove.class )
+	local PosTrash = self:GetPos() + Vector(math.random(1,2), math.random(1,2), 0)
+
+	TrashEnt:SetPos( PosTrash )
+	TrashEnt:Spawn()
+	TrashEnt:SetModel(TrashToRemove.model)
+	TrashEnt:Health(TrashToRemove.health)
+	TrashEnt:Activate()
+
+	self.ActualCapacity = self.ActualCapacity - 1
+	table.remove( self.Trash ) -- Remove the last element.
+end
+
+
+function ENT:Initialize()
+	self:SetModel( "models/scp_207/scp_207.mdl" )
+	self:RebuildPhysics()
+end
+
+function ENT:RebuildPhysics( )
+	self:PhysicsInit( SOLID_VPHYSICS ) 
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid( SOLID_VPHYSICS ) 
+	self:SetUseType(SIMPLE_USE)
+	self:PhysWake()
+end
+
+function ENT:PhysicsCollide( data, physobj )
+	if ( data.Speed > 250 and data.DeltaTime > 0.01) then
+		sound.Play( PhysicSoundHeavy, self:GetPos(), 75, math.random( 50, 160 ) )	
+	elseif (data.Speed > 50 and data.DeltaTime > 0.01) then
+		sound.Play( PhysicSoundLow, self:GetPos(), 75, math.random( 50, 160 ) )	
+	end
+end
+
+function ENT:OnTakeDamage( dmginfo )
+	local DmgReceive = dmginfo:GetDamage()
+	if (DmgReceive >= 30) then
+		sound.Play( PhysicSoundHeavy, self:GetPos(), 75, math.random( 50, 160 ) )	
+	else
+		sound.Play( PhysicSoundLow, self:GetPos(), 75, math.random( 50, 160 ) )	
+	end
+end
+
+function ENT:Use(ply)
+	if(!IsValid(ply) or self.ActualCapacity == 0) then return end
+
+	self:RemoveTrash()
+	sound.Play( OpenSoundGarbage, self:GetPos(), 75, math.random( 50, 160 ) )	
+end
+
+-- TODO : Faire les sons de jeter en fonction du type de ce qui a été jeté ?
+function ENT:Touch(ent)
+	if (!IsValid(ent) or self.ActualCapacity == self.MaxCapacity) then return end
+
+	self:AddTrash(ent)
+	sound.Play( TrowSoundGarbage, self:GetPos(), 75, math.random( 50, 160 ) )	
+end
